ff9cd097251dbb1ac1a4de54b82a4c0f18ef42e8
Ryan Wang <wanglun02@gmail.com>
2017-09-11 01:04:52 +0800

    删除tags

--- schoolapp/views.py
+++ schoolapp/views.py
@@ -4,7 +4,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics, viewsets
 from rest_framework.permissions import IsAdminUser
-from collections import OrderedDict
 
 
 from .serializers import *
